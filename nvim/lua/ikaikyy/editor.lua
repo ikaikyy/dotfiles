05ea7caa923151dda7b63543390e943cf10442c3
Kaiky Eduardo <kaikyeduardomf@gmail.com>
2024-05-03 19:32:23 -0300

    feat(nvim): make background transparentOD

--- nvim/lua/ikaikyy/editor.lua
+++ nvim/lua/ikaikyy/editor.lua
@@ -6,3 +6,17 @@ vim.bo.softtabstop = 2
 vim.opt.relativenumber = true
 
 vim.opt.clipboard = "unnamedplus"
+
+-- Make background transparent
+
+local groups = {
+  'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+  'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+  'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+  'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+  'EndOfBuffer'
+}
+
+for _, group in ipairs(groups) do
+  vim.cmd('hi! ' .. group .. ' guibg=NONE ctermbg=NONE')
+end
