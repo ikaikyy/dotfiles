90073b869d7de12424ef6bf27740b3ffa30c460e
Kaiky Eduardo <kaikyeduardomf@gmail.com>
2025-03-04 09:39:55 -0300

    feat(nvim): add setup to copilot.lua plugin

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,7 +1,41 @@
 require('copilot').setup({
   panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom",
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    hide_during_completion = false,
+    debounce = 75,
     keymap = {
       accept = "<C-p>",
+      accept_word = false,
+      accept_line = false,
+      next = "<C-]>",
+      prev = "<C-[>",
+      dismiss = "<esc>",
     },
-  }
+  },
+  filetypes = {
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node',
+  server_opts_overrides = {},
 })
