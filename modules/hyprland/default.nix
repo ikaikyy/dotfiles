d4d387d55417319d8a5d97724a84ee943f5beefa
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-05-21 10:59:45 -0300

    feat: finish hyprland configuration

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -25,6 +25,48 @@
         sensitivity = -0.5;
       };
 
+      general = {
+        gaps_out = 12;
+        border_size = 0;
+
+        layout = "dwindle";
+
+        allow_tearing = false;
+      };
+
+      decoration = {
+        rounding = 12;
+
+        blur = {
+          enabled = true;
+          size = 5;
+          passes = 2;
+        };
+
+      };
+
+      animations = {
+        enabled = "yes";
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = "yes";
+        preserve_split = "yes";
+      };
+
+      gestures = { workspace_swipe = "off"; };
+
       bind = [
         "CTRL ALT, T, exec, kitty"
         "$mod, Q, killactive, "
