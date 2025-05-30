23238b998b9c6e306d7fe76153a6eee6a9829b65
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-05-30 20:56:17 -0300

    feat(zsh): add powerlevel10k zsh theme

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -1,4 +1,6 @@
 { config, pkgs, ... }: {
+  home.packages = with pkgs; [ zsh-powerlevel10k ];
+
   programs.zsh = {
     enable = true;
 
@@ -16,9 +18,18 @@
         "sudo nix flake update --flake ~/dotfiles && update-laptop --upgrade";
     };
 
+    initContent = ''
+      if [[ -r "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+        source "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
+      fi
+
+      if [[ -r "$HOME/.p10k.zsh" ]]; then
+        source "$HOME/.p10k.zsh"
+      fi
+    '';
+
     oh-my-zsh = {
       enable = true;
-      theme = "agnoster";
       plugins = [
         "colored-man-pages"
         "command-not-found"
