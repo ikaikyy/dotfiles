94a1c778a0c62cf8354754a7508e5a753a50d30e
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-05-30 13:31:32 -0300

    feat(ags): redesign workspaces widget

--- modules/ags/config/widget/Bar/HyprlandWorkspaces.tsx
+++ modules/ags/config/widget/Bar/HyprlandWorkspaces.tsx
@@ -26,15 +26,37 @@ function changeWorkspace(workspaceID: number) {
 }
 
 function Workspace({ workspaceID }: WorkspaceProps) {
+  const cssClasses = Variable.derive([activeWorkspace], (activeWorkspace) => {
+    const isEmpty = !hyprland
+      .get_workspaces()
+      .some(
+        (workspace) =>
+          workspace.id === workspaceID && workspace.clients.length > 0,
+      );
+
+    return [
+      "hyprland-workspace",
+      activeWorkspace === workspaceID ? "active" : "",
+      isEmpty ? "empty" : "",
+    ];
+  });
+
   return (
     <box
-      cursor={Gdk.Cursor.new_from_name("pointer", null)}
-      cssClasses={[
-        "hyprland-workspace",
-        activeWorkspace.get() === workspaceID ? "active" : "",
-      ]}
-      onButtonPressed={() => changeWorkspace(workspaceID)}
-    />
+      heightRequest={16}
+      widthRequest={16}
+      halign={Gtk.Align.CENTER}
+      valign={Gtk.Align.CENTER}
+    >
+      <box
+        cursor={Gdk.Cursor.new_from_name("pointer", null)}
+        cssClasses={cssClasses()}
+        hexpand={true}
+        halign={Gtk.Align.CENTER}
+        valign={Gtk.Align.CENTER}
+        onButtonPressed={() => changeWorkspace(workspaceID)}
+      />
+    </box>
   );
 }
 
@@ -42,16 +64,14 @@ export default function HyprlandWorkspaces() {
   return (
     <box
       cssClasses={["hyprland-workspace-list"]}
+      halign={Gtk.Align.CENTER}
       valign={Gtk.Align.CENTER}
-      spacing={8}
+      hexpand_set={true}
+      spacing={4}
     >
-      {activeWorkspace().as(() => {
-        const workspaces = Array.from({ length: 9 }, (_, index) => (
-          <Workspace workspaceID={index + 1} />
-        ));
-
-        return workspaces;
-      })}
+      {Array.from({ length: 9 }, (_, index) => (
+        <Workspace workspaceID={index + 1} />
+      ))}
     </box>
   );
 }
