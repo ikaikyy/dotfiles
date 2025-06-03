44f5f7c4e4dac66c326e25201e00024994ddcf25
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-06-02 21:06:44 -0300

    (ags): search for tray icons on IconTheme

--- modules/ags/config/widget/Bar/SystemTray.tsx
+++ modules/ags/config/widget/Bar/SystemTray.tsx
@@ -1,8 +1,10 @@
 import { bind } from "astal";
-import { Gtk, astalify } from "astal/gtk4";
+import { Gdk, Gtk, astalify } from "astal/gtk4";
 import Tray from "gi://AstalTray";
 
 const PopoverMenu = astalify(Gtk.PopoverMenu);
+
+const IconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
 
 export default function SystemTray() {
   const tray = Tray.get_default();
@@ -10,31 +12,68 @@ export default function SystemTray() {
   return (
     <box cssClasses={["system-tray"]} valign={Gtk.Align.CENTER}>
       {bind(tray, "items").as((items) =>
-        items.map((item) => (
-          <menubutton
-            cssClasses={["system-tray-item"]}
-            setup={(self) => {
-              self.insert_action_group("dbusmenu", item.actionGroup);
-
-              bind(self, "active").subscribe((active) => {
-                if (active) {
-                  self.add_css_class("active");
-                } else {
-                  self.remove_css_class("active");
-                }
-              });
-            }}
-            tooltipText={bind(item, "tooltipMarkup")}
-          >
-            <image gicon={bind(item, "gicon")} valign={Gtk.Align.CENTER} />
-            <PopoverMenu
+        items.map((item) => {
+          return (
+            <menubutton
+              cssClasses={["system-tray-item"]}
               setup={(self) => {
-                self.set_menu_model(item.menuModel);
-                self.add_css_class("system-tray-item-popover");
+                self.insert_action_group("dbusmenu", item.actionGroup);
+
+                bind(self, "active").subscribe((active) => {
+                  if (active) {
+                    self.add_css_class("active");
+                  } else {
+                    self.remove_css_class("active");
+                  }
+                });
               }}
-            />
-          </menubutton>
-        )),
+              tooltipText={bind(item, "tooltipMarkup")}
+            >
+              <image
+                paintable={bind(item, "iconName").as((iconName) => {
+                  let iconPaintable: Gtk.IconPaintable;
+
+                  if (iconName) {
+                    iconPaintable = IconTheme.lookup_icon(
+                      iconName,
+                      null,
+                      16,
+                      1,
+                      null,
+                      null,
+                    );
+                  } else if (item.gicon) {
+                    iconPaintable = IconTheme.lookup_by_gicon(
+                      item.gicon,
+                      16,
+                      1,
+                      null,
+                      null,
+                    );
+                  } else {
+                    iconPaintable = IconTheme.lookup_icon(
+                      "application-x-executable",
+                      null,
+                      16,
+                      1,
+                      null,
+                      null,
+                    );
+                  }
+
+                  return iconPaintable;
+                })}
+                valign={Gtk.Align.CENTER}
+              />
+              <PopoverMenu
+                setup={(self) => {
+                  self.set_menu_model(item.menuModel);
+                  self.add_css_class("system-tray-item-popover");
+                }}
+              />
+            </menubutton>
+          );
+        }),
       )}
     </box>
   );
