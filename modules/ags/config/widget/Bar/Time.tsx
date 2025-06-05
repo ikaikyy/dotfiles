d560cd43c7c3b732ccb6fecb6ec5c42e7f7736df
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-06-04 21:34:22 -0300

    use ags config as a git submodule

--- modules/ags/config/widget/Bar/Time.tsx
+++
@@ -1,8 +0,0 @@
-import { Gtk } from "astal/gtk4";
-import { Variable } from "astal";
-
-const datetime = Variable("").poll(1000, "date '+%H:%M:%S - %d de %B'");
-
-export default function Time() {
-  return <label cssClasses={["time"]} label={datetime()} />;
-}
