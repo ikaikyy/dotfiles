a85883bae0d78c8d3bb9eed3583140ec193abdc0
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-07-03 13:56:00 -0300

    feat(neovim): add copilot plugin

--- modules/neovim/plugins.nix
+++ modules/neovim/plugins.nix
@@ -143,5 +143,48 @@
     };
 
     markdown-preview.enable = true;
+
+    copilot-lua = {
+      enable = true;
+      settings = {
+        panel = {
+          enabled = true;
+          auto_refresh = false;
+          keymap = {
+            jump_prev = "[[";
+            jump_next = "]]";
+            accept = "<CR>";
+            refresh = "gr";
+            open = "<M-CR>";
+          };
+          layout = {
+            position = "bottom";
+            ratio = 0.4;
+          };
+        };
+        filetypes = {
+          gitcommit = false;
+          gitrebase = false;
+          hgcommit = false;
+          svn = false;
+          cvs = false;
+          "." = false;
+        };
+        suggestion = {
+          enabled = true;
+          auto_trigger = true;
+          hide_during_completion = false;
+          debounce = 75;
+          keymap = {
+            accept = "<C-p>";
+            accept_word = false;
+            accept_line = false;
+            next = "<C-]>";
+            prev = "<C-[>";
+            dismiss = "<esc>";
+          };
+        };
+      };
+    };
   };
 }
