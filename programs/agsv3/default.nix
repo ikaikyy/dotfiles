f8ddff0736b00f1141a658b0f3147ad9b46c9868
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-07-23 09:08:25 -0300

    feat: init base ags project

---
+++ programs/agsv3/default.nix
@@ -0,0 +1,34 @@
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [inputs.ags.homeManagerModules.default];
+
+  programs.ags = {
+    enable = true;
+
+    configDir = ./config;
+
+    extraPackages = [
+      inputs.ags.packages.${pkgs.system}.apps
+      inputs.ags.packages.${pkgs.system}.hyprland
+      inputs.ags.packages.${pkgs.system}.tray
+      inputs.ags.packages.${pkgs.system}.battery
+    ];
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    exec-once = ["ags run --gtk4"];
+    bind = ["$mod, M, exec, ags request 'toggle-app-launcher'"];
+    layerrule =
+      builtins.concatMap
+      (s: ["blur, ${s}" "blurpopups, ${s}" "ignorezero, ${s}"]) [
+        "AppLauncher"
+        "PowerMenu"
+        "Bar"
+      ];
+  };
+}
