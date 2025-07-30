c74c7909579133b5bc9f9750799708bef9ee91e5
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-07-30 11:33:32 -0300

    rollback(neovim): add formatting keymap back

--- programs/neovim/lua/config/keymaps.lua
+++ programs/neovim/lua/config/keymaps.lua
@@ -6,3 +6,8 @@ keymap.set("n", "<leader>sv", "<Cmd>w<CR>", { desc = "Save buffer" })
 
 -- Clear search highlight
 keymap.set("n", "<Esc>", "<Cmd>noh<CR>", { desc = "Clear search highlight" })
+
+-- Formatting
+keymap.set({ "n", "v" }, "<leader>fmt", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format buffer" })
