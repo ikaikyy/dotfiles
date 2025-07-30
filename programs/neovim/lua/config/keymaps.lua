53859048bc09a614e0afaeb3ee88207b3a7191ec
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-07-30 09:56:57 -0300

    (neovim): remove fmt keymap as saving buffer has the same behavior

--- programs/neovim/lua/config/keymaps.lua
+++ programs/neovim/lua/config/keymaps.lua
@@ -6,6 +6,3 @@ keymap.set("n", "<leader>sv", "<Cmd>w<CR>", { desc = "Save buffer" })
 
 -- Clear search highlight
 keymap.set("n", "<Esc>", "<Cmd>noh<CR>", { desc = "Clear search highlight" })
-
--- Formatting
-keymap.set({ "n", "v" }, "<leader>fmt", "<Cmd>Format<CR>", { desc = "Format buffer" })
