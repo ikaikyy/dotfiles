5e6b3dfcb1bf45ee9efe2ea2a9f4e13f5963779b
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2025-08-30 16:23:49 -0300

    chore(agsv3): remove unneeded code

--- programs/agsv3/default.nix
+++ programs/agsv3/default.nix
@@ -4,8 +4,9 @@
   pkgs,
   lib,
   ...
-}: {
-  imports = [inputs.ags.homeManagerModules.default];
+}:
+{
+  imports = [ inputs.ags.homeManagerModules.default ];
 
   programs.ags = {
     enable = true;
@@ -16,19 +17,22 @@
       inputs.ags.packages.${pkgs.system}.apps
       inputs.ags.packages.${pkgs.system}.hyprland
       inputs.ags.packages.${pkgs.system}.tray
-      inputs.ags.packages.${pkgs.system}.battery
     ];
   };
 
   wayland.windowManager.hyprland.settings = {
-    exec-once = ["ags run --gtk4"];
-    bind = ["$mod, M, exec, ags request 'toggle-app-launcher'"];
+    exec-once = [ "ags run --gtk4" ];
+    bind = [ "$mod, M, exec, ags request 'toggle-app-launcher'" ];
     layerrule =
       builtins.concatMap
-      (s: ["blur, ${s}" "blurpopups, ${s}" "ignorezero, ${s}"]) [
-        "AppLauncher"
-        "PowerMenu"
-        "Bar"
-      ];
+        (s: [
+          "blur, ${s}"
+          "blurpopups, ${s}"
+          "ignorezero, ${s}"
+        ])
+        [
+          "AppLauncher"
+          "Bar"
+        ];
   };
 }
