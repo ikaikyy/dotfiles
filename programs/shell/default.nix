32a93e0192d5ed596a681bdc89a3cc14e62ca2e2
Kaiky Eduardo Martins de Faria <kaikyeduardomf@gmail.com>
2026-05-02 14:16:17 -0300

    Revert "feat(shell): add dotfiles commit helper"
    This reverts commit 4ea15e51b4174175d77d0dfa6e1cd85bd1bdc49a.

--- programs/shell/default.nix
+++ programs/shell/default.nix
@@ -12,9 +12,6 @@
   fzf-tmux-new-window = pkgs.writeShellScript "fzf-tmux-new-window" (
     builtins.readFile ./scripts/fzf-tmux-new-window.sh
   );
-  dotfiles-commit = pkgs.writeShellScript "dotfiles-commit" (
-    builtins.readFile ./scripts/dotfiles-commit.sh
-  );
 in {
   home.packages = with pkgs; [
     ghostty
@@ -22,7 +19,6 @@ in {
     direnv
     fd
     fzf
-    dotfiles-commit
   ];
 
   home.file.".config/ghostty/config".source = ./ghostty.config;
@@ -38,11 +34,6 @@ in {
       # Laptop
       update-laptop = ''sudo nixos-rebuild switch --flake "$HOME/dotfiles?submodules=1#nixos-laptop" --sudo'';
       upgrade-laptop = ''sudo nix flake update --flake "$HOME/dotfiles" && update-laptop --upgrade'';
-
-      dot = ''git -C "$HOME/dotfiles"'';
-      dotst = ''git -C "$HOME/dotfiles" status -sb'';
-      dotlg = ''git -C "$HOME/dotfiles" log --oneline -10'';
-      dotcommit = "${dotfiles-commit}";
 
       tmux = "tmux -2";
     };
